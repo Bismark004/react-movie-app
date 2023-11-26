95e197dda808e1bc17090f2d0d8131f1117e4cdd
Bismark Adzai <adzaibismark@gmail.com>
2023-11-26 00:52:47 +0000

    .....

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'get from themoviedb.org',
+    apiKey: '8daab53e806e1a389e43ee17bc9cdff8',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
